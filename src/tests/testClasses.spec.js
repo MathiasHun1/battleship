a0322ffed20e20c4e6372252cf7d56aef1e155c3
MathiasHun <mathias.hun@gmail.com>
2024-04-15 23:58:36 +0200

    painfully implemented some working methods

--- src/tests/testClasses.spec.js
+++ src/tests/testClasses.spec.js
@@ -1,4 +1,5 @@
 import { Ship, GameBoard } from "../modules/classes";
+import {containsSameElement, checkAllEmpty, makeField} from "../modules/helperFunctions";
 
 describe('Ship', () => {
     test('initialize properties correctly', () => {
@@ -29,26 +30,12 @@ describe('GameBoard', () => {
         expect(myBoard.width).toBe(50)
         expect(myBoard.height).toBe(50)
     })
-    test('places ship horizontally', () => {
-        myBoard.setShip1('tiny', 2, 'horizontal', [0, 0])
-        expect(myBoard.ship1[1]).toStrictEqual([[0, 0], [0, 1]])
-    })
-    test('places ship vertically', () => {
-        myBoard.setShip2('bigger', 3, 'vertical', [13, 22])
-        expect(myBoard.ship2[1]).toStrictEqual([[13, 22], [14, 22], [15, 22]])
-    })
-    test('throw error if place ship outside the board', () => {
-        expect(() => { myBoard.setShip('golem', 5, 'vertical', [46, 13]) }).toThrow('board limits reached')
-        expect(() => { myBoard.setShip('very big', 4, 'horizontal', [11, 48]) }).toThrow('board limits reached')
-    })
-    test('checks if a field is available', () => {
+    test('setShipHorizontal works', () => {
+        myBoard.setShipHorizontal('big', 5, [0, 0])
+        expect(myBoard.ships[0].name).toBe('big')
+        expect(myBoard.checkEmptyField([0, 0])).toBe(false)
         expect(myBoard.checkEmptyField([0, 5])).toBe(true)
-        myBoard.takenFields.push([12, 12])
-        expect(myBoard.checkEmptyField([12, 12])).toBe(false)
+
     })
-    test('not places new ship if the spot has been taken', () => {
-        myBoard.setShip1('bla', 6, 'horizontal', [0, 15])
-        expect(() => { myBoard.setShip('golem', 5, 'horizontal', [0, 13]) }).toThrow('spot has been taken')
-        expect(myBoard.checkEmptyField([0, 14])).toBe('true')
-    })
+    
 })
