15a0e10e1f67a66c60f33eac107fc0f9ae928e4c
MathiasHun <mathias.hun@gmail.com>
2024-04-15 15:58:53 +0200

    .

--- src/modules/classes.js
+++ src/modules/classes.js
@@ -2,8 +2,8 @@
 
 class Ship {
     constructor (name, length) {
-        this.name = name
-        this.length = length
+        this.name = name.trim()
+        this.length = parseInt(length)
         this.timesHit = 0
     }
 
@@ -16,15 +16,41 @@ class Ship {
     }
 }
 
+// gameboard class
 
-
-// ship factory
-const shipFactory = (name, length) => {
-    const timesHit = 0
-    const isSunk = () => {
-        return timesHit >= length
+class GameBoard {
+    constructor(name, height, width) {
+        this.name = name
+        this.height = height
+        this.width = width
+        this.ship1 = ''
+        this.ship2 = ''
+    }
+    
+    //init the ships position
+    setShip1(direction, startField, size) {
+        this.ship1 = [...this.setShip(direction, startField, size)]
+    }
+    
+    setShip2(direction, startField, size) {
+        this.ship2 = [...this.setShip(direction, startField, size)]
     }
 
-    return { name, length, timesHit, isSunk}
+    //additional methods
+    setShip(direction, startField, size) {
+        const arr = []
+        if (direction === 'horizontal') {
+            for(let i = 0; i < size; i++) {
+                arr.push([startField[0], startField[1] + i])
+            }
+        } else if (direction === 'vertical') {
+            for(let i = 0; i < size; i++) {
+                arr.push([startField[0] + i, startField[1]])
+            }
+        }
+        return arr
+    }
 }
 
+
+export { Ship, GameBoard }
