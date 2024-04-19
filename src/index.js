f03a5c69ad2ff401d214433b70b72c12db0225f7
MathiasHun <mathias.hun@gmail.com>
2024-04-19 15:27:42 +0200

    files only for pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "./modules/controller";
-
-Controller.initStartView()
-const starButton = document.querySelector('.start-button')
-starButton.addEventListener('click', () => {
-    Controller.initGame()
-    starButton.style.display = 'none'
-})
